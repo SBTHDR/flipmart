7be894656a1334ebed01a27daaaf9ecd004f18ee
SBTHDR <subrata.halder13d@gmail.com>
2021-06-23 21:57:19 +0600

    dashboard profile name

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Welcome <strong>{{ Auth::user()->name }}</strong>
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+            <h1>Dashboard Content</h1>
         </div>
     </div>
 </x-app-layout>
